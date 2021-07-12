d62cdcabe75c56449170f16d912c998c5196dfbb
woot <worrawutp@gmail.com>
2021-07-12 16:37:33 +0700

    Define hero power base on the job

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -7,8 +7,14 @@ class Hero < ApplicationRecord
 
   def initialize_profile
     self.level = 1
-    self.hp = 50
-    self.mp = 0
     self.job = HeroJob.default if job.blank?
+    
+    init_hp_mp_by_job
+  end
+
+  def init_hp_mp_by_job
+    hero_job = HeroJob.new(job)
+    self.hp = hero_job.init_hp
+    self.mp = hero_job.init_mp
   end
 end
--- app/models/hero_job.rb
+++ app/models/hero_job.rb
@@ -1,9 +1,36 @@
 class HeroJob
+  BASE_HP = 100
+  BASE_MP = 70
+
   def self.available_jobs
     %w(squire knight white_mage black_mage monk)
   end
 
   def self.default
     'squire'
+  end
+
+  def initialize(title)
+    @title = title
+  end
+
+  def init_hp
+    case @title
+    when 'squire'     then BASE_HP * 1.0 
+    when 'knight'     then BASE_HP * 1.25
+    when 'white_mage' then BASE_HP * 0.85
+    when 'black_mage' then BASE_HP * 0.9
+    when 'monk'       then BASE_HP * 1.33
+    end
+  end
+
+  def init_mp
+    case @title
+    when 'squire'     then BASE_MP * 0
+    when 'knight'     then BASE_MP * 0
+    when 'white_mage' then BASE_MP * 1.35
+    when 'black_mage' then BASE_MP * 1.25
+    when 'monk'       then BASE_MP * 0
+    end
   end
 end
