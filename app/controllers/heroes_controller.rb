e1c932af15ab0dfa6eacb757e7a8f7e141993c75
woot <worrawutp@gmail.com>
2021-07-13 23:23:54 +0700

    Handle invalid job

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -5,16 +5,11 @@ class HeroesController < ApplicationController
   end
 
   def create
-    if hero_name_missing? || find_duplicated_hero?
-      render_error("cannot create hero. the hero name may be missing or duplicated")
-      return
-    end
-
     hero = Hero.new(hero_create_params)
     if hero.save
       render json: hero
     else
-      render_error("cannot create hero")
+      render_error("cannot create hero: #{hero.errors.messages}")
     end
   end
 
@@ -55,18 +50,5 @@ class HeroesController < ApplicationController
 
   def find_hero
     Hero.find_by_id params[:id]
-  end
-
-  def hero_name_missing?
-    return true if params[:hero].blank?
-    return true if params[:hero][:name].blank?
-  end
-
-  def find_duplicated_hero?
-    input_name = params[:hero][:name]
-    hero = Hero.where(name: input_name).last
-
-    return false if hero.blank?
-    input_name.downcase == hero.name.downcase
   end
 end
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,7 +1,11 @@
 class Hero < ApplicationRecord
   before_create :initialize_profile
 
-  enum gender: { male: "male", female: "female" }
+  enum gender: { male: "m", female: "f" }
+
+  validates :name, presence: true
+  validates :name, uniqueness: true, on: :create
+  validate :with_existing_job, on: :create
 
   private
 
@@ -16,5 +20,13 @@ class Hero < ApplicationRecord
     hero_job = HeroJob.new(job)
     self.hp = hero_job.init_hp
     self.mp = hero_job.init_mp
+  end
+
+  private
+
+  def with_existing_job
+    if HeroJob.available_jobs.exclude?(job)
+      errors.add(:job, "is invalid")
+    end
   end
 end
