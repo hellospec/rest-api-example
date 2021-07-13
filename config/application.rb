b92c4876761835c62a912f008d4b214b1e58cd51
woot <worrawutp@gmail.com>
2021-07-13 19:26:38 +0700

    Add allow host for Rails app

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module ApiExample
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts << "restapitest.hellospec.work"
   end
 end
