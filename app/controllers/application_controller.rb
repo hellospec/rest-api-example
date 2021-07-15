3a03e87556835577519a9bbd2d315af8c5ccbbad
woot <worrawutp@gmail.com>
2021-07-15 09:48:52 +0700

    Add basic authenticate handler in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    username, passwd = ActionController::HttpAuthentication::Basic.user_name_and_password(request)
+
+    user = User.find_by_username(username)
+    if user.blank? || !user.authenticate(passwd)
+      render json: { error: "not authenticated" }, status: :unauthorized
+    end
+
+    @current_user = user
+  rescue
+    render json: { error: "not authenticated" }, status: :unauthorized
+  end
 end
